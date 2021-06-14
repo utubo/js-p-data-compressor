d15909f8ec14fb303776e7d1bca149eae5acd7c0
utubo <utbutb@gmail.com>
2021-06-14 20:31:23 +0900

    first commit

---
+++ p-data-compressor.js
@@ -0,0 +1,64 @@
+// p-data-compressor
+// Copyright (c) 2021 utubo under the [MIT](https://opensource.org/licenses/mit-license.php)
+{
+	let checkLen = s => {if (500 < s.length) throw 'too long';};
+	let symbols = "ghijklmnopqrstuvwxyzABCDEFGHIJKLNMOPRSTUVWXYZ";
+	var pdcEncode = str => {
+		checkLen(str);
+		let MAX_REPEAT = symbols.length;
+		let data = str.split('');
+		let out = '';
+		let l = str.length;
+		let i = 0;
+		while (i < l) {
+			let c = data[i];
+			let ss = str.slice(0, i);
+			let from = i - MAX_REPEAT + 1;
+			let head;
+			let len = 0;
+			for (let j = 2; j <= l-i; j ++) {
+				let k = ss.indexOf(str.substr(i, j), from);
+				if (k === -1) break;
+				head = ss.length - k;
+				len = j;
+			}
+			if (2 < len) {
+				out += symbols[len] + symbols[head];
+			} else {
+				out += c;
+				len = 1;
+				while (c === data[i + len] && len < MAX_REPEAT) len ++;
+				if (2 < len) {
+					out += symbols[len - 1] + symbols[0];
+				} else {
+					len = 1;
+				}
+			}
+			i += len;
+		}
+		return out;
+	};
+	var pdcDecode = data => {
+		checkLen(data);
+		let out = '';
+		let len;
+		for (let c of data) {
+			checkLen(out);
+			let symbol = symbols.indexOf(c);
+			if (symbol === -1) {
+				out += c;
+			} else if (len) {
+					if (symbol) {
+						out += out.substr(out.length - symbol, len);
+					} else {
+						out += out[out.length - 1].repeat(len);
+					}
+					len = 0;
+			} else {
+				len = symbol;
+			}
+		}
+		return out;
+	};
+}
+
---
+++ readme.md
@@ -0,0 +1,27 @@
+
+This is a compressor for https://utb.sakura.ne.jp/tekitou/p
+This compresses a text matches /^[0-9a-f,]+$/.
+
+## Usage
+```javascript
+const str = '12345,11111,12345';
+const data = pzl.enc('str')
+// -> '12345,1kg,ls'
+
+pdcDecode(data);
+// -> '12345,11111,12345';
+```
+
+## Customize
+
+You can search and fix the source code to customize settings.
+
+### Max length
+Default: 500
+
+### Reserved symboles
+Default: `ghijklmnopqrstuvwxyzABCDEFGHIJKLNMOPRSTUVWXYZ`
+
+## Lisense
+Copyright (c) 2021 utubo under the [MIT](https://opensource.org/licenses/mit-license.php)
+
