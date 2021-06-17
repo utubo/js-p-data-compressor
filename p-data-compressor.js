f45595c5aaebf77607b8b8f39c88e251c8d0c81d
utubo <utbutb@gmail.com>
2021-06-17 19:44:42 +0900

    refactoring

--- p-data-compressor.js
+++ p-data-compressor.js
@@ -5,17 +5,16 @@
 	let symbols = "ghijklmnopqrstuvwxyzABCDEFGHIJKLNMOPRSTUVWXYZ";
 	var pdcEncode = str => {
 		checkLen(str);
-		let MAX_REPEAT = symbols.length;
-		let data = str.split('');
+		let MAX_RANGE = symbols.length;
+		let STR_LENGTH = str.length;
 		let out = '';
-		let l = str.length;
 		let i = 0;
-		while (i < l) {
+		while (i < STR_LENGTH) {
 			let ss = str.slice(0, i);
-			let from = i - MAX_REPEAT + 1;
+			let from = i - MAX_RANGE;
 			let head;
 			let len;
-			for (let j = 3; j <= l-i; j ++) {
+			for (let j = 3; j <= STR_LENGTH - i; j ++) {
 				let k = ss.indexOf(str.substr(i, j), from);
 				if (k === -1) break;
 				head = ss.length - k;
@@ -24,10 +23,10 @@
 			if (len) {
 				out += symbols[len] + symbols[head];
 			} else {
-				let c = data[i];
+				let c = str[i];
 				out += c;
 				len = 1;
-				while (c === data[i + len] && len < MAX_REPEAT) len ++;
+				while (c === str[i + len] && len < MAX_RANGE) len ++;
 				if (2 < len) {
 					out += symbols[len - 1] + symbols[0];
 				} else {
